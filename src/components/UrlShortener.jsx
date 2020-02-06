b8e35ce92c0abea1f666e229f38f9c99783ba10f
Davion Teh <6722889+davionboy@users.noreply.github.com>
2020-02-06 23:59:54 +0800

    Formatted document

--- src/components/UrlShortener.jsx
+++ src/components/UrlShortener.jsx
@@ -10,37 +10,38 @@ export const UrlShortener = (props) => {
     const handleOnUrlSubmit = (url) => {
         //API call here
         fetch(`${API_LINK}/shorten`, {
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
             },
-            body:JSON.stringify({url})
+            body: JSON.stringify({ url })
         })
-        .then(response => {
-            if(!response.ok){
-                throw new Error(response.json());
-            }
-                
-            return response.json();
-        })
-        .then(body => {
-            //change Urls state
-            console.log(body);
-            setLink(body.link);
-        })
-        .catch(error => {
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.json());
+                }
+
+                return response.json();
+            })
+            .then(body => {
+                //change Urls state
+                console.log(body);
+                setLink(body.link);
+            })
+            .catch(error => {
                 //handle bad response
-        })
-        .finally(() => {
-            //clean up UI
-        })
+            })
+            .finally(() => {
+                //clean up UI
+            })
     }
 
+
     return (
-        <div>
+        <div className="urlshortener">
             <input type="text" className="text-longUrl" placeholder="Enter your long URL here!" />
             <br />
-            <input type="button" onClick={(e)=>handleOnUrlSubmit(document.querySelector(".text-longUrl").value)} value="Shorten Url!" />
+            <input type="button" onClick={(e) => handleOnUrlSubmit(document.querySelector(".text-longUrl").value)} value="Shorten Url!" />
             <br />
             <div>
                 {(
@@ -56,11 +57,11 @@ export const UrlShortener = (props) => {
                     )
                         : false
                 )}
-            {
+                {
                     copiedLink ? <span>Copied!</span> :
                         false
-            }
-        </div>
+                }
+            </div>
 
         </div>
     )
