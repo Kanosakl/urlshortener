da8476bd6f40aa93477da25e87501150467d57d3
Davion Teh <6722889+davionboy@users.noreply.github.com>
2020-02-06 15:00:47 +0800

    Added cookie session

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require('dotenv-safe').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 
 const app = express();
 
+app.use(cookieSession({
+    maxAge: 1000 * 60 * 60 * 24 * 14, //2 weeks expiry day,
+    keys: process.env.SESSION_KEY
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false}));
 
