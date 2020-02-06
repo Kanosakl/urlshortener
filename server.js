322af38b56423da265476fe2dbd1cb72c66347aa
Davion Teh <6722889+davionboy@users.noreply.github.com>
2020-02-06 16:00:59 +0800

    Added simple Bitly API

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const cookieSession = require('cookie-session');
 
 const app = express();
 
+const DEFAULT_API_ROUTE = "/api/v1";
+const bitlyRoute = require('./server/bitlyAPI');
+
 app.use(cookieSession({
     maxAge: 1000 * 60 * 60 * 24 * 14, //2 weeks expiry day,
     keys: process.env.SESSION_KEY
@@ -12,6 +15,7 @@ app.use(cookieSession({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false}));
 
+app.use(`${DEFAULT_API_ROUTE}/bitly/`, bitlyRoute.router);
 app.use('/',express.static('./dist'));
 
 app.listen(process.env.PORT);
---
+++ server/bitlyAPI.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+
+const BITLY_API_PATH = `${process.env.BITLY_API_PATH}/v4`;
+let bitlyGroupGUID;
+
+fetch(`${BITLY_API_PATH}/groups`)
+    .then(response => response.json())
+    .then(body => {
+        if (body.groups && body.groups.length)
+            bitlyGroupGUID = body.groups[0].guid;
+
+        router.use('/', (req, res, next) => {
+            if (!bitlyGroupGUID) {
+                res
+                    .status(500)
+                    .json({
+                        data: {
+                            description: "BitlyAPI not ready yet",
+                            hint: "Try again later"
+                        }
+                    })
+            }
+
+            next();
+        })
+
+        router.post('/shorten', (req, res, next) => {
+            const { url } = req.body;
+
+            try {
+                new URL(url);
+            } catch (error) {
+                res
+                    .status(400)
+                    .json({
+                        data: {
+                            description: "Invalid URL provided",
+                            hint: "Please make sure url is a fully qualified url, e.g. https://www.example.com"
+                        }
+                    });
+                return;
+            }
+
+            fetch(`${BITLY_API_PATH}/shorten`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${process.env.BITLY_API_TOKEN}`
+                },
+                body: JSON.stringify({
+                    "group_guid": bitlyGroupGUID,
+                    "domain": "bit.ly",
+                    "long_url": url
+                })
+            })
+                .then(response => {
+                    if (!response.ok)
+                        throw new Error({
+                            statusCode: response.statusCode,
+                            body: response.json()
+                        });
+
+                    return response.json();
+                })
+                .then(body => {
+                    const { link, long_url, created_at } = body;
+                    res.json({ link, long_url, created_at });
+                    return;
+                })
+
+        })
+
+
+    })
+
+module.exports = {
+    router,
+}
