7cf564d654e0594c97a16b5c637dcf5ffa34abe2
Davion Teh <6722889+davionboy@users.noreply.github.com>
2020-02-06 16:09:43 +0800

    Added catch all error handling

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 const DEFAULT_API_ROUTE = "/api/v1";
 const bitlyRoute = require('./server/bitlyAPI');
 
+//TODO: logging using Winston
+//TODO: server API testing using Mocha 
+
 app.use(cookieSession({
     maxAge: 1000 * 60 * 60 * 24 * 14, //2 weeks expiry day,
     keys: process.env.SESSION_KEY
@@ -17,6 +20,19 @@ app.use(bodyParser.urlencoded({ extended:false}));
 
 app.use(`${DEFAULT_API_ROUTE}/bitly/`, bitlyRoute.router);
 app.use('/',express.static('./dist'));
+
+//handle all error
+app.use((error,req,res,next) => {
+    console.logs(error);
+    res
+    .status(500)
+    .json({
+        data:{
+            description:`Something went wrong :/`,
+            hint:`TODO`
+        }
+    })
+})
 
 app.listen(process.env.PORT);
 console.log(`listening at ${process.env.PORT}`);
